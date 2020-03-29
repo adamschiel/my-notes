8ee48628206df4686b037c2e57b069277c610a78
adamschiel <adamschiel@yahoo.com>
2020-03-29 10:55:42 -0600

    Updates

---
+++ precache-manifest.1fdc79e70addc649d0e13b933f7c1ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d0447e210d0e22c27bee9f08013757",
+    "url": "/my-notes/index.html"
+  },
+  {
+    "revision": "a6f01a96d059079332bd",
+    "url": "/my-notes/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4a234acf792323dc2d72",
+    "url": "/my-notes/static/js/2.9b26e30b.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/my-notes/static/js/2.9b26e30b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f01a96d059079332bd",
+    "url": "/my-notes/static/js/main.2c5b049b.chunk.js"
+  },
+  {
+    "revision": "b0aef3d23c22e9ea6975",
+    "url": "/my-notes/static/js/runtime-main.ea06106c.js"
+  }
+]);
